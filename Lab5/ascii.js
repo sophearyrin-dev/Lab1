0ef924a84847dfe6c175aa22a9fb85746015896b
sophearyrin1 <srin@miu.edu>
2022-05-01 10:09:28 -0500

    update ex

--- Lab5/ascii.js
+++ Lab5/ascii.js
@@ -1,55 +1,70 @@
-function animationOnChange() {
-    document.getElementById("text-area").value = "";
-    var x = document.getElementById("animation").value;
-    console.log(x);
-    document.getElementById("text-area").value = ANIMATIONS[x];
-}
 
-let currentAnimation;
-let customeAnimation = ["Blank", "Exercise", "Juggler", "Bike", "Dive"];
-var myInterval;
-let t;
-function startAnimation() {
-    t = 250;
-    currentAnimation = document.getElementById("text-area").value;
+let timer = null;
+let content = "";
+let speed = 250;
+let i;
+let args;
 
+
+function onStart () {
+
+    if (timer != null ) clearInterval(timer);
+    content = document.getElementById("text-area").value;
     document.getElementById("stop").disabled = false;
+    document.getElementById("start").disabled = true;
+    let scenerio = document.getElementById("animation");
+    scenerio.disabled = true;
+    args = ANIMATIONS[scenerio.value].split("=====");
+    let argsLength = args.length; i = 0;
+    let start = Date.now(); 
 
-    let count = 0;
-
-    myInterval = setInterval(function () {
-
-        document.getElementById("text-area").value = ANIMATIONS[customeAnimation[count++]];
-
-        if (count == 5) count = 0;
-
-    }, speedChange());
-
+    timer = setInterval(function() {
+        let timePassed = Date.now() - start;
+        if (i === argsLength ) i = 0;
+        draw(timePassed, args[i]); i++;
+      }, speed );
 }
 
-function speedChange() {
-    const cb = document.querySelector('#turbo');
-    if (cb.checked == true) {
-        return 50;
-    }
-    return 250;
+function draw(time, draft) {
+    document.getElementById("text-area").value = draft;
 }
+function onChoose () {
+    
+    if (timer != null ) clearInterval(timer);
+    content = document.getElementById("text-area").value;
+    let args = ANIMATIONS[this.value];
+    document.getElementById("text-area").value = args;
 
-function stopAnimation() {
-
-    clearInterval(myInterval);
-
+}
+function onStop () {
+    clearInterval(timer);
+    document.getElementById("text-area").value = content;
+    document.getElementById("start").disabled = false;
     document.getElementById("stop").disabled = true;
-
-    document.getElementById("text-area").value = currentAnimation;
-
+    document.getElementById("animation").disabled = false;
+}
+function onCheck () {
+    clearInterval(timer);
+    speed = document.getElementById("turbo").checked == true ?  50 : 2500;
+    let start = Date.now(); 
+    timer = setInterval(function() {
+        let timePassed = Date.now() - start;
+        let argsLength = args.length;
+        if (i === argsLength ) i = 0;
+        draw(timePassed, args[i]); i++;
+      }, speed );
 }
 
 function fontSizeOnChange() {
     let fontSize = document.getElementById("fontsize").value;
     document.getElementById("text-area").style.fontSize = fontSize;
 }
+window.onload = function() {
+    let startButt = document.getElementById("start");
+    startButt.onclick = onStart;
+    document.getElementById("stop").onclick = onStop;
+    document.getElementById("animation").onchange = onChoose;
+    document.getElementById("turbo").onchange = onCheck;
+    document.getElementById("fontsize").onchange = onFontSizeSelected;
 
-
-
-
+}
