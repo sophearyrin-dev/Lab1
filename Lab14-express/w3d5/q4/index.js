dea71a1d8eb459e7bca84297fc5f669bd2df55c4
sophearyrin1 <srin@miu.edu>
2022-05-12 18:41:00 -0500

    update css path

--- Lab14-express/w3d5/q4/index.js
+++ Lab14-express/w3d5/q4/index.js
@@ -5,11 +5,12 @@ const path = require("path");
 const date = new Date();
 const hour = date.getHours();
 
+console.log(hour);
 if(hour >=6 && hour <18){
     app.use(express.static(path.join(__dirname + '/public')));
 }
-else if(hour>=18 && hour <6) {
-    app.use(express.static(path.join(__dirname + '/public/css')));
+else {
+    app.use(express.static(path.join(__dirname + '/public/css/')));
 }
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -35,5 +36,3 @@ app.listen(3000, () => {
     //all of your code are here, this is what will happen in server
     console.log('Running server on port : 3000 .....');
 });
-
-
