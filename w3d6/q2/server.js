ec83023bdaa8425892a5c77d6cae5d4693aad307
sophearyrin1 <srin@miu.edu>
2022-05-16 00:20:26 -0500

    update

--- w3d6/q2/server.js
+++ w3d6/q2/server.js
@@ -3,15 +3,25 @@ const express = require('express');
 const app = express();
 const path = require("path");
 
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname , 'templates/views'));
 app.use(bodyParser.urlencoded({extended:false}));
-app.get('/', (request, response, next) => {
-    response.sendFile(path.join(__dirname + '/template/view/index.ejs'));
+
+app.get('/', (req, res, next) => {
+    res.render("home", {
+        titlepage : 'Login'
+    });
 })
 
 app.post('/result', (req, res, next)=>{
-    let name = req.body.username;
-    let myage = req.body.age;
-    res.send(`Welcome ${name}, age ${myage}`);
+    var info = {
+        name : req.body.username,
+        myage:req.body.age
+    }
+    res.render('result', {
+        infoValue : info,
+        titlepage : 'My Info'
+    })
 })
 
 
